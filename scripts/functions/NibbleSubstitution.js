46eb9159be15bc056a69ca797839db7eba873955
blackarez <angellg1990@gmail.com>
2023-06-06 18:05:17 -0500

    se unifica la estructura de metodos existentes

--- scripts/functions/NibbleSubstitution.js
+++ scripts/functions/NibbleSubstitution.js
@@ -1,16 +1,28 @@
-const nibbleSubstitution = (decimals = 0, sBox = []) => {
-    let bin = convertToBinary(decimals);
-    let result=decimals;
-    //binario de 4 digitos
-    bin = bin.padStart(4, "0");
-    //console.log("bin:"+bin);
-    const column = binaryToDecimal(bin.slice(0,2));
-    //console.log("col:"+column);
-    const row = binaryToDecimal(bin.slice(2,4));
-    //console.log("row:"+row);
-    //busca el elemento en la carta correspondiente
-    result = sBox[column][row];
-    //console.log(result);
-    return result;
-}
+const nibbleSubstitution = (arrayBytes = [], sBox = []) => {
+  //"arrayBytes" array de 4 elementos
+  let result = [];
+  //console.log(arrayBytes);
+  const column = binaryToDecimal(`${arrayBytes[0]}${arrayBytes[1]}`);
+  //console.log("col:"+column);
+  const row = binaryToDecimal(`${arrayBytes[2]}${arrayBytes[3]}`);
+  //console.log("row:"+row);
+  //busca el elemento en la caja correspondiente
+  result = convertToBinary(sBox[column][row]);
+  result = Array.from(result.padStart(4, "0"));
+  //console.log("result:");
+  //console.log(result);
+  return result;
+};
 
+const processNibbleSubstitution = (arrayBytes = [], sBox = []) => {
+  let result = [];
+  //console.log([arrayBytes.slice(0, 4)]);
+  result = result.concat(
+    ...nibbleSubstitution(arrayBytes.slice(0, 4), sBox),
+    ...nibbleSubstitution(arrayBytes.slice(4, 8), sBox),
+    ...nibbleSubstitution(arrayBytes.slice(8, 12), sBox),
+    ...nibbleSubstitution(arrayBytes.slice(12), sBox)
+  );
+  //console.log(result);
+  return result;
+};
--- scripts/functions/ShiftRows.js
+++ scripts/functions/ShiftRows.js
@@ -1,18 +1,11 @@
-const shiftRowsBinario = (arrBinary =[]) => {
-    let result = [];
-    //se intercambian los bloques
-    result = result.concat(
-        ...arrBinary.slice(0, 4), 
-        ...arrBinary.slice(12), 
-        ...arrBinary.slice(8, 12), 
-        ...arrBinary.slice(4, 8));
-    return result;
-}
-
-const shiftRowsDecimal = (arrDecimal =[]) => {
-    //los decimales del array es de 0 - 15
-    let result = [];
-    //se intercambian los bloques de 4 bit
-    result = result.concat(...arrDecimal[0], ...arrDecimal[3], ...arrDecimal[2], ...arrDecimal[1]);
-    return result;
-}
+const shiftRowsBinario = (arrBytes = []) => {
+  let result = [];
+  //se intercambian los bloques
+  result = result.concat(
+    ...arrBytes.slice(0, 4),
+    ...arrBytes.slice(12),
+    ...arrBytes.slice(8, 12),
+    ...arrBytes.slice(4, 8)
+  );
+  return result;
+};
