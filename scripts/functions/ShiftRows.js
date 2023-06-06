430f237a50d16f7c26307a4d310f80c36c614f05
blackarez <angellg1990@gmail.com>
2023-06-06 10:25:12 -0500

    se implementa funcionamiento shifRows con valores decimales

--- scripts/functions/ShiftRows.js
+++ scripts/functions/ShiftRows.js
@@ -1,4 +1,4 @@
-function shiftRows(arrKeyBinary =[]) {
+function shiftRowsBinario(arrBinary =[]) {
     let result = [];
     const auxA = [];
     const auxB = [];
@@ -6,10 +6,19 @@ function shiftRows(arrKeyBinary =[]) {
     const auxD = [];
     //se separan en bloques de 4 elementos "bits" 
     //para poder trabajar con ellos y intercambiarlo
-    auxA.push(...arrKeyBinary.slice(0, 4));
-    auxB.push(...arrKeyBinary.slice(4, 8));
-    auxC.push(...arrKeyBinary.slice(8, 12));
-    auxD.push(...arrKeyBinary.slice(12));
+    auxA.push(...arrBinary.slice(0, 4));
+    auxB.push(...arrBinary.slice(4, 8));
+    auxC.push(...arrBinary.slice(8, 12));
+    auxD.push(...arrBinary.slice(12));
+    //se intercambian los bloques
     result = result.concat(auxA, auxD, auxC, auxB);
+    return result;
+}
+
+function shiftRowsDecimal(arrDecimal =[]) {
+    //los decimales del array es de 0 - 15
+    let result = [];
+    //se intercambian los bloques de 4 bit
+    result = result.concat(...arrDecimal[0], ...arrDecimal[3], ...arrDecimal[2], ...arrDecimal[1]);
     return result;
 }
