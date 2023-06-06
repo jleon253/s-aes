76340d63ebe3a7cc7e89c5b5158aebd95ee40c14
blackarez <angellg1990@gmail.com>
2023-06-06 09:18:41 -0500

    comentarios shiftRow

--- scripts/functions/ShiftRows.js
+++ scripts/functions/ShiftRows.js
@@ -4,6 +4,8 @@ function shiftRows(arrKeyBinary =[]) {
     const auxB = [];
     const auxC = [];
     const auxD = [];
+    //se separan en bloques de 4 elementos "bits" 
+    //para poder trabajar con ellos y intercambiarlo
     auxA.push(...arrKeyBinary.slice(0, 4));
     auxB.push(...arrKeyBinary.slice(4, 8));
     auxC.push(...arrKeyBinary.slice(8, 12));
