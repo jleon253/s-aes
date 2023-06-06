f6d143f9ac1b78d0c249940b50382a468fb5e972
jleon253 <jleon253@gmail.com>
2023-06-05 23:02:45 -0500

    utilities added to utils.js

---
+++ scripts/utils.js
@@ -0,0 +1,41 @@
+// https://www.utilities-online.info/ascii-to-hex
+const AsciiToHex = (str = '') => {
+  return str.split('').map((val) => Number(val.charCodeAt(0)).toString(16));
+}
+
+// https://www.binaryhexconverter.com/hex-to-binary-converter
+const HexToBinary = (hex = []) => {
+  return hex.map((val) => parseInt(val, 16).toString((2)).padStart(8, '0'))
+}
+
+// https://binarytotext.net/binary-to-hexadecimal/
+const BinaryToHex = (bin = []) => {
+  return bin.map((val) => parseInt(val, 2).toString(16).toUpperCase())
+}
+
+// https://www.rapidtables.com/convert/number/hex-to-ascii.html
+const HexToAscii = (hex = []) => {
+  const arrHEXNum = hex.map((val) => parseInt(val, 16))
+  const msgASCII = String.fromCharCode(...arrHEXNum)
+
+  return msgASCII;
+}
+
+
+// -------- XOR Functions ------------
+
+// https://xor.pw/#
+const XORbyBit = (b1 = '', b2 = '') => {
+  return `${b1 ^ b2}`;
+}
+
+// https://xor.pw/#
+const XORbyByte = (B1 = [], B2 = []) => {
+  const arrXOR = [];
+
+  for(let i = 0; (i < B1.length) && (i < B2.length); i++) {
+    arrXOR.push(XORbyBit(B1[i], B2[i]));
+  }
+
+  return arrXOR;
+}
