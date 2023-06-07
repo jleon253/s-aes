c85e93b0cccbf37960706f8d9a16897165b3b6ea
blackarez <angellg1990@gmail.com>
2023-06-06 20:04:44 -0500

    la base de mix Column

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,24 @@
 /**
  * msg y clave son de 16 bits : 2 caracteres
  */
-const msg = '×(';
-const key = 'Jõ';
+const msg = "×(";
+const key = "Jõ";
 
 /**
  * se crean arrays que contienen todos los bits
  * msgBinary = [1,0,0,1,0,1,0,1,1,0]
  */
-const msgBinary = HexToBinary(AsciiToHex(msg)).join('').split('');
-const keyBinary = HexToBinary(AsciiToHex(key)).join('').split('');
+const msgBinary = HexToBinary(AsciiToHex(msg)).join("").split("");
+const keyBinary = HexToBinary(AsciiToHex(key)).join("").split("");
+
+/* La primera ronda de encriptacion*/
+const roundA = (key) => {
+    const info = shiftRows(processNibbleSubstitution(key, sBox));
+    mixColumn(info, boxMix, objectMixColumn);
+};
 
 /**
  * Se invoca la función addKey
  */
 const key0 = addKey(msgBinary, keyBinary);
-shiftRows(processNibbleSubstitution(key0, sBox));
+roundA(key0);
